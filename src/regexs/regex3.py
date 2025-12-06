f594fc5c8b57ebd66c7672d9b145791be86bdac4
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-12-06 18:43:07 +0530

    zore or one time

--- src/regexs/regex3.py
+++ src/regexs/regex3.py
@@ -4,3 +4,10 @@ text="ab abb abbbb a aa abc acb"
 pat=re.compile(r"ab+")
 all_matching_text=re.findall(pat,text)
 print(all_matching_text)
+
+# zero or one time (?)
+
+text1='ab abb a a a abbbb abbbbbb ac ca'
+pat1=re.compile('ab?')
+one_or_more_time=re.findall(pat1,text1)
+print(one_or_more_time)
