917e4153328a55ef774fc6f08cab3f3b9acc2e64
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-12-10 22:01:15 +0530

    more than one object will be addd

--- src/misc/scaler/Dec_12/car.py
+++ src/misc/scaler/Dec_12/car.py
@@ -15,8 +15,19 @@ class Car:
     def __str__(self):
         return f"📌 name of car => {self.name} and giving milage is {self.milage} 📌"
     
-    def __add__(self, object2):
-        return self.milage + object2.milage
+    def __add__(self, other):
+        # if isinstance(other, Car):
+        #     return self.milage + other.milage
+        # return NotImplemented
+        return Car(
+                name=f"{self.name}+{other.name}",
+                milage=self.milage + other.milage
+            )
+    
+    def __radd__(self,other):
+        if other==0:
+            return self
+        return self.__add__(other)
     
     def __lt__(self,object2):
         return self.milage < object2.milage
@@ -28,4 +39,11 @@ c=Car('THAR',20)
 print(c)
 d=Car('limorgini',120)
 print(c+d)
-print(c<d)
+#print(c<d)
+#print(c>d)
+e=Car('TATA',200)
+print([c,d,e])
+#print(result)
+print(c+d)
+print(d+e)
+print(c+d+e)
