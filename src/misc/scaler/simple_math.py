6b51dde4ce462c9791e5da16f3b5a8c9872dda66
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-12-10 19:10:54 +0530

    add

---
+++ src/misc/scaler/simple_math.py
@@ -0,0 +1,10 @@
+'''
+Problem Description
+Perform the following operations on 6 and 3 and print their respective outputs in different lines.
+
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Double Division
+
+'''
