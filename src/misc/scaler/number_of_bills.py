1cf039a0ab0114ca9e54926ad89f11671a0a34c4
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-12-10 19:23:23 +0530

    no of bills problem abs number and explained

--- src/misc/scaler/number_of_bills.py
+++ src/misc/scaler/number_of_bills.py
@@ -11,7 +11,15 @@ Output Format
 Print in a single line denoting the total number of bills that can fit in the total budget.
 
 '''
+'''
+Example Explanation:
+
+Here, the bill amount is 126.3 and the value of a single bill is 5. 
+Hence, total number of bills that can fit in the total budget as whole number, 
+will be 126.3 divided by 5 which returns 25.
+Note: The problem constraints mean that when we test your code, the test cases used in the backend can have input values only within those constraints. You need not implement them in your code. You need to make sure your code will work for all such input values!
+'''
 N=float(input())
 M=int(input())
 bills=N//M
-print(bills)
+print(int(bills))
