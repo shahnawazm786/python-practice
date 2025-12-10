7ac162b152c6777539ad693e8f3d02bdbb5b3ce0
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-12-10 18:54:23 +0530

    hello name

---
+++ src/misc/scaler/hello_name.py
@@ -0,0 +1,15 @@
+'''
+Problem Description
+
+Take a name A as input from the user and print "Hello A", where A is the name in input.
+
+Problem Constraints
+
+1 <= len(A) <= 15 Characters in A are in lowercase English Alphabets.
+Input Format
+
+There is a single input line, which is the string **A**.
+Output Format
+
+Print in a single line "Hello A" without quotes.
+'''
