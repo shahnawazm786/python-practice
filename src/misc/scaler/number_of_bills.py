8e31893d3bc06d5b5747434b1c48479279c4a3d4
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-12-10 19:19:31 +0530

    no of bills problem

---
+++ src/misc/scaler/number_of_bills.py
@@ -0,0 +1,13 @@
+'''
+Problem Description
+
+Given total bills amount and amount of a single bill. Print number of bills.
+Input Format
+
+The first line contains a real number N denoting the total budget.
+The second line contains an integer M denoting the value of a single bill.
+Output Format
+
+Print in a single line denoting the total number of bills that can fit in the total budget.
+
+'''
