33592e0bff1acada102ea4db893e97f77fc2c7f2
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-12-10 18:42:28 +0530

    say hi

---
+++ src/misc/scaler/say_hi.py
@@ -0,0 +1,10 @@
+'''
+Problem Description
+Take two names A and B as input from the user, print "A says Hi to B" (Without quotations), where A and B are the names in input.
+
+Problem Constraints
+
+1 <= len(A), len(B) <= 15
+Characters in A and B are in lowercase English Alphabets.
+
+'''
