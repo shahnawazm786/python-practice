5ae96360071696af0f13b8105aaa9dccb78c7847
Mohammad Shahnawaz <msnazia2012@gmail.com>
2026-01-15 22:35:03 +0530

    numpy

--- maths/dav-01/numpy_01.py
+++ maths/dav-01/numpy_01.py
@@ -1,6 +1,13 @@
 import numpy as np
+import time
 a=[1,2,3,4,5]
 res= [i**2 for i in a]
 print(res)
 arr=np.array(a)
 print(arr**2)
+l=range(100000)
+start=time.time()
+res1=[i**2 for i in a]
+print(res1)
+end=time.time()
+print(end-start,'seconds')
