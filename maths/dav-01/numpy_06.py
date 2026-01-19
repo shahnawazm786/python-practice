f11d7d8f4abb5f56c76e48b697c8dfe785dcae94
Mohammad Shahnawaz <msnazia2012@gmail.com>
2026-01-19 11:36:33 +0530

    numpy scaler

--- maths/dav-01/numpy_06.py
+++ maths/dav-01/numpy_06.py
@@ -1,1 +1,12 @@
 import numpy as np
+
+def reverse_columns(arr):
+    arr = np.array(arr)
+    return arr[:, ::-1]
+
+arr = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+]
+print(reverse_columns(arr))
