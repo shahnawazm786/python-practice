38bed561583604e4cd7c4ffa6f2fd98cb32dd7b9
Mohammad Shahnawaz <msnazia2012@gmail.com>
2026-01-31 14:58:26 +0530

    assignment

---
+++ educ/pandas/class_02/assignment/solutions/Q7.py
@@ -0,0 +1,19 @@
+'''
+Which of the following line of code will throw an error?
+
+Use the code below in order to create the given dataframe:
+'''
+import pandas as pd
+
+data = {
+    'cust_id': [101, 102, 103, 104],
+    'name': ['rick', 'morty', 'pickle', 'jerry'],
+}
+
+df = pd.DataFrame(data)
+print(df)
+print(df.isin([101,102,120]))
+print(df.isin({'cust_id':[101,102,120]}))
+print(df.isin(df.cust_id))
+print(df.cust_id)
+print(df.name.isin('rick'))
